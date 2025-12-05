a06cc38c5c513fe2adaf9f94b3c8e5bdcb14a2c5
shema-salvator <shemavatorsalvator@gmail.com>
2025-12-05 15:54:00 +0200

    modifications

--- index.js
+++ index.js
@@ -5,11 +5,13 @@ const port = 3000;
 
 const server = http.createServer((req, res) => {
   // Check if the requested URL is /about
-  if (req.url === '/about') {
+  if (req.url === '/about')
+         {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     res.end('About Page\n'); // Response for the /about route
-  } else {
+  } 
+  else {
     // Default response for all other routes (e.g., / or anything else)
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
